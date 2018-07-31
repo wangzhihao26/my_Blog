7e84dbfacbe5862c97d32ba3670b03dd3ad71eb7
root <root@host.localdomain>
2018-07-31 12:23:25 -0400

    add for deployment

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a+5f2uopivf^mg5hllpz%op-^)b9mad8%8bwm2nb$13m2z=6u6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zhihaoblog.net']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/blogApp/my_Blog/blog/static/' 
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
+from django.urls import include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path(r'', include('blog.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'', include('blog.urls')),
 ]
