e364701a4d84e6ad3993e3cf2551e3401b00ea10
wangzhihao26 <250847909@qq.com>
2018-08-01 00:48:24 +0800

    add requirement file

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path, re_path
-<<<<<<< HEAD
 from . import views
 
 
-=======
-from .import views
->>>>>>> 86001fc87a2ed11c41a311988000a3a7f95a8531
 app_name = 'blog'
 urlpatterns = [
     path(r'', views.index, name='index'),
     re_path(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> 86001fc87a2ed11c41a311988000a3a7f95a8531
+
