b1ba5b0797655b891a8eb605a55d70ed12294d26
wangzhihao26 <250847909@qq.com>
2018-08-04 02:04:27 +0800

    add tag

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path(r'', views.IndexView.as_view(), name='index'),
     re_path(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
     re_path(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
+    re_path(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
     re_path(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
 
